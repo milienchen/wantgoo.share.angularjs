db18b023c1b581879b8ff31856ceed612d6e244f
supershowwei <supershowwei@gmail.com>
2017-02-02 23:19:28 +0800

    complete ng-repeat sample

--- AngularJSDemo/AngularJSDemo/js/home/indexController.js
+++ AngularJSDemo/AngularJSDemo/js/home/indexController.js
@@ -14,5 +14,11 @@
             self.showFullName = function () {
                 alert(self.firstName + ", " + self.lastName);
             };
+
+            self.users = [
+                { firstName: "Johnny", lastName: "Chuang", age: 18 },
+                { firstName: "Kevin", lastName: "Chen", age: 8 },
+                { firstName: "Mary", lastName: "Chou", age: 9 }
+            ];
         }
     ]);
